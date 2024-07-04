e4ecdb807fd2618b284ab746595adfa8b84e71e7
Anne-SophieNehls <anne-sophie.nehls@t-online.de>
2024-07-04 17:02:53 +0200

    16 update 17 uhr

--- 2024_07_04_Tag16/string methods/assets/String-Methods-Level-1_11.js
+++ 2024_07_04_Tag16/string methods/assets/String-Methods-Level-1_11.js
@@ -13,3 +13,8 @@ document.write(z);
 let n = x + " " + y;
 
 document.write( "Ich bin Erste:r" + " Ich komme auf Platz zwei")
+
+let ersterString = "Ich bin Erste:r";
+let zweiterString = "Ich komme auf Platz zwei";
+let gemeinsamerString = ersterString +  zweiterString;
+document.write(gemeinsamerString);
